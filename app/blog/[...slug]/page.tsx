a4951cbca7068293dc18d39c38161199ac7431f0
Ahmd Mufahras Li Alfazh Assardew <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-04-14 22:29:52 +0700

    fix: slug page

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -2,9 +2,13 @@ import fs from "fs";
 import matter from "gray-matter";
 
 import { MDXRemote } from "next-mdx-remote/rsc";
+import path from "path";
 
 function getPost({ slug }: { slug: string }) {
-    const source = fs.readFileSync(`./app/posts/${slug}.mdx`, "utf8");
+    const source = fs.readFileSync(
+        path.join("./app/posts", `${slug}.mdx`),
+        "utf8"
+    );
     const { content, data } = matter(source);
 
     return { content, data };
@@ -14,7 +18,7 @@ export default function Blog({ params }: { params: { slug: string } }) {
     const props = getPost(params);
     return (
         <div className="px-4 pt-32">
-            <h1 className="font-bold text-4xl">Blog</h1>
+            <h1 className="font-bold text-4xl">{props.data.title}</h1>
             <MDXRemote source={props.content} />
         </div>
     );
