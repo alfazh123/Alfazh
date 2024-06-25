c898bffada88aa85b155b6a7dee2822a2a235624
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2024-06-25 19:26:04 +0700

    fix: width and height art card

--- app/components/art-photo/art-card-date.tsx
+++ app/components/art-photo/art-card-date.tsx
@@ -20,13 +20,13 @@ export default function ArtCard({ art, date }: { art: art; date: date }) {
 
     return (
         <div className="flex flex-col items-center justify-center w-full h-full">
-            <div className="w-full h-full cursor-pointer" onClick={toggleModal}>
+            <div className="lg:w-56 lg:h-56 w-52 h-52 cursor-pointer" onClick={toggleModal}>
                 <Image
                     src={art.image}
                     alt={art.image}
                     width={500}
                     height={500}
-                    className="md:rounded-md rounded-sm"
+                    className="md:rounded-lg rounded-md object-cover object-center w-full h-full"
                 />
             </div>
 
@@ -35,13 +35,13 @@ export default function ArtCard({ art, date }: { art: art; date: date }) {
                     className="fixed top-0 left-0 bottom-0 right-0 w-full h-full bg-slate300 dark:bg-slate800 dark:bg-opacity-60 bg-opacity-60 backdrop-blur-2xl flex items-center justify-center"
                     onClick={toggleModal}
                 >
-                    <div className="relative flex flex-col aspect-auto bg-white md:px-4 md:py-8 px-2 py-4 md:space-y-2">
+                    <div className="relative flex flex-col w-96 bg-white md:px-4 md:py-8 px-4 py-6 space-y-2 mx-5">
                         <Image
                             src={art.image}
                             alt={art.image}
                             width={500}
                             height={500}
-                            className="rounded-sm w-full"
+                            className="rounded-sm h-full"
                             onClick={(e) => e.stopPropagation()}
                         />
                         <p className="text-slate900 font-semibold md:text-base text-sm">
--- app/components/art-photo/list-art.tsx
+++ app/components/art-photo/list-art.tsx
@@ -10,7 +10,8 @@ interface ListArtProps {
 export default function ListArt({ Props }: ListArtProps) {
     return (
         <div>
-            <div className="lg:columns-4 sm:columns-3 columns-2 gap-4 space-y-4">
+            {/* <div className="lg:columns-4 sm:columns-3 columns-2 gap-4 space-y-4"> */}
+            <div className="flex flex-wrap gap-4 justify-center">
                 {Props.art.map((art, id) => (
                     <div className={``} key={id}>
                         <ArtCard
